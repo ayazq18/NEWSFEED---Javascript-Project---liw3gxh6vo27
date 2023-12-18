8653d0ecc73fb9f65b6b35c13283d15679af6029
ayazq18@gmail.com <131868270+ayazq18@users.noreply.github.com>
2023-12-18 18:57:08 +0530

    in progress

--- App.js
+++ App.js
@@ -1,25 +1,121 @@
+const fetchData = async () => {
+  const result = await fetch(
+    "https://content.newtonschool.co/v1/pr/64e3d1b73321338e9f18e1a1/inshortsnews"
+  );
+  return await result.json();
+};
 
-const fetchData = async ()=>{
-    const result = await fetch("https://content.newtonschool.co/v1/pr/64e3d1b73321338e9f18e1a1/inshortsnews")
-    const data = await result.json()
-    return data
+function getNewsFromLocalStorage() {
+  const savedNews = JSON.parse(localStorage.getItem("savedNews"));
+  return savedNews === null ? [] : savedNews;
 }
 
-const newData = document.getElementById('newNews')
-newData.innerHTML = ''
+function addNewsToLocalStorage(newsName) {
+  let savedNewsName = [];
+  savedNewsName = localStorage.getItem("savedNews");
+  savedNewsName = savedNewsName ? JSON.parse(savedNewsName) : [];
+  savedNewsName.push(newsName);
+  const updatedStorage = JSON.stringify(savedNewsName);
+  localStorage.setItem("savedNews", updatedStorage);
+}
 
-const loadBtn = document.getElementById('loadNewBtn')
-loadBtn.addEventListener('click', async ()=>{
-    const data = await fetchData()
-    
-    const randomIndex = Math.floor(Math.random() * data.length)
-    const randomNews = data[randomIndex]
-    
-        const newDiv =  document.createElement('div')
-        newDiv.innerHTML = `
-        <h1 id="new-author">By ${randomNews.author}</h1>
-        <p id="new-content">${randomNews.content}</p>
-        <a id="new-url" href="${randomNews.url}">Read More.</a>
-        `
-        newData.appendChild(newDiv)
+function removeNewsFromLocalStorage(newsName) {
+  const savedNewsName = getNewsFromLocalStorage();
+  localStorage.setItem(
+    "savedNews",
+    JSON.stringify(
+      savedNewsName.filter((newsNam) => newsNam.author !== newsName.author)
+    )
+  );
+}
+
+//newNews
+const renderNews = (data) => {
+  const newData = document.getElementById("newNews");
+  newData.innerHTML = "";
+
+  data.map((news) => {
+    const newDiv = document.createElement("div");
+    const { author, category, content, url } = news;
+    newDiv.innerHTML += `
+          <div class = "newNews-content">
+            <div class = "newAuthor-category">
+                <h2 id="new-author"><p>By</p> ${author}</h2>
+                <h2 id="new-author"><p>Category</p> ${category}</h2>
+            </div>
+            <div class = "newNews-content-url">
+                <p id="new-content">${content}</p>
+                <a id="new-url" href="${url}">Read More.</a>
+            </div>
+            <i class="fa fav-icon fa-regular fa-heart fa-2xl transparent"></i>
+          </div>
+          `;
+    newData.appendChild(newDiv);
+    const favIcon = newDiv.querySelector(".fa-heart");
+    favIcon.addEventListener("click", () => {
+      if (favIcon.classList.contains("transparent")) {
+        addNewsToLocalStorage(news);
+        favIcon.classList.toggle("transparent", false);
+      } else {
+        removeNewsFromLocalStorage(news);
+        favIcon.classList.toggle("transparent", true);
+      }
+    });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const data = await fetchData();
+    renderNews(data);
+    const filterBtn = document.querySelectorAll(".filterBtn");
+    filterBtn.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        const category = btn.id;
+        renderFilteredNews(category);
+      });
+    });
+    const renderFilteredNews = (category) => {
+      const filteredNews = data.filter(
+        (news) => news.category.toLowerCase() === category.toLowerCase()
+      );
+      renderNews(filteredNews);
+    };
+
+    const filterall = document.getElementById("all");
+    filterall.addEventListener("click", () => {
+      renderNews(data);
+    });
+
+    document.querySelector(".floppy").addEventListener('click', ()=>{
+      window.location.href = 'savedNews.html'
+    })
 });
+
+//saveNews
+const savedNewsList = getNewsFromLocalStorage();
+  savedNewsList.map((news) => {
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = `
+        <div class="newNews-content">
+          <div class="newAuthor-category">
+            <h2 id="new-author"><p>By</p> ${news.author}</h2>
+            <h2 id="new-author"><p>Category</p> ${news.category}</h2>
+          </div>
+          <div class="newNews-content-url">
+            <p id="new-content">${news.content}</p>
+            <a id="new-url" href="${news.url}">Read More.</a>
+          </div>
+          <i class="fas fa-times-circle xmark"></i>
+        </div>
+      `;
+    savedNews.appendChild(newDiv);
+    const favIcon = newDiv.querySelector(".xmark");
+    favIcon.addEventListener("click", () => {
+      removeNewsFromLocalStorage(news);
+      newDiv.remove();
+    });
+  });
+
+  document.querySelector(".new-icon").addEventListener('click', ()=>{
+    window.location.href = 'newNews.html'
+  })
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # html-css-js-project-boilerplate
+# html, css, for the dashboard
+# JS functionality for Load new News button
+# JS functionality for Load saved news button
+# JS functinality for the News category buttons
+# Navigation to the new news page and saved news page
