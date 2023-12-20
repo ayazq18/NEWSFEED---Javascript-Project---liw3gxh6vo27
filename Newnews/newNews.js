c2862e038efd0a3156252eb58b3b2f766319056a
ayazq18@gmail.com <131868270+ayazq18@users.noreply.github.com>
2023-12-20 17:02:33 +0530

    updated

---
+++ Newnews/newNews.js
@@ -0,0 +1,119 @@
+const fetchData = async () => {
+  const result = await fetch(
+    "https://content.newtonschool.co/v1/pr/64e3d1b73321338e9f18e1a1/inshortsnews"
+  );
+  return await result.json();
+};
+
+function getNewsFromLocalStorage() {
+  const savedNews = JSON.parse(localStorage.getItem("savedNews"));
+  return savedNews === null ? [] : savedNews;
+}
+
+function addNewsToLocalStorage(newsName) {
+  let savedNewsName = [];
+  savedNewsName = localStorage.getItem("savedNews");
+  savedNewsName = savedNewsName ? JSON.parse(savedNewsName) : [];
+  savedNewsName.push(newsName);
+  const updatedStorage = JSON.stringify(savedNewsName);
+  localStorage.setItem("savedNews", updatedStorage);
+}
+
+function removeNewsFromLocalStorage(newsName) {
+  const savedNewsName = getNewsFromLocalStorage();
+  localStorage.setItem(
+    "savedNews",
+    JSON.stringify(
+      savedNewsName.filter((newsNam) => newsNam.author !== newsName.author)
+    )
+  );
+}
+//newNews
+
+const renderNews = (data) => {
+  const newData = document.getElementById("newNews");
+  newData.innerHTML = "";
+  const getStoredData = getNewsFromLocalStorage();
+  data.map((news) => {
+    const newDiv = document.createElement("div");
+    const { author, category, content, url } = news;
+    newDiv.innerHTML += `
+              <div cladd="newNews-section">
+                <div class = "newNews-content">
+                  <div class = "newAuthor-category">
+                      <h2 id="new-author"><p>By</p> ${author}</h2>
+                      <h2 id="new-author"><p>Category</p> ${category}</h2>
+                  </div>
+                  <div class = "newNews-content-url">
+                      <p id="new-content">${content} <a id="new-url" href="${url}">Read More.</a></p>
+                  </div>
+                  <i class="fa fav-icon fa-regular fa-heart fa-2xl transparent"></i>
+                </div>
+              </div>
+                `;
+    newData.appendChild(newDiv);
+    const favIcon = newDiv.querySelector(".fa-heart");
+
+    if (getStoredData.some((savedNews) => savedNews.author === news.author)) {
+      favIcon.classList.remove("transparent");
+    }
+
+    favIcon.addEventListener("click", () => {
+      if (favIcon.classList.contains("transparent")) {
+        addNewsToLocalStorage(news);
+        favIcon.classList.remove("transparent");
+      } else {
+        removeNewsFromLocalStorage(news);
+        favIcon.classList.add("transparent");
+      }
+    });
+  });
+};
+
+const showLoader = () => {
+  document.getElementById("loader").style.display = "block";
+};
+
+const hideLoader = () => {
+  document.getElementById("loader").style.display = "none";
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loadingData = async () => {
+    showLoader();
+
+    try {
+      const data = await fetchData();
+      renderNews(data);
+
+      const filterBtn = document.querySelectorAll(".filterBtn");
+      filterBtn.forEach((btn) => {
+        btn.addEventListener("click", () => {
+          const category = btn.id;
+          renderFilteredNews(category);
+        });
+      });
+
+      const renderFilteredNews = (category) => {
+        const filteredNews = data.filter(
+          (news) => news.category.toLowerCase() === category.toLowerCase()
+        );
+        renderNews(filteredNews);
+      };
+
+      const filterall = document.getElementById("all");
+      filterall.addEventListener("click", () => {
+        renderNews(data);
+      });
+
+      document.querySelector(".flop-save").addEventListener("click", () => {
+        window.location.href = "../Savednews/savedNews.html";
+      });
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+      hideLoader();
+  };
+
+  loadingData();
+});
---
+++ Savednews/savedNews.js
@@ -0,0 +1,78 @@
+const fetchData = async () => {
+    const result = await fetch(
+      "https://content.newtonschool.co/v1/pr/64e3d1b73321338e9f18e1a1/inshortsnews"
+    );
+    return await result.json();
+  };
+  
+  function getNewsFromLocalStorage() {
+    const savedNews = JSON.parse(localStorage.getItem("savedNews"));
+    return savedNews === null ? [] : savedNews;
+  }
+  
+  function addNewsToLocalStorage(newsName) {
+    let savedNewsName = [];
+    savedNewsName = localStorage.getItem("savedNews");
+    savedNewsName = savedNewsName ? JSON.parse(savedNewsName) : [];
+    savedNewsName.push(newsName);
+    const updatedStorage = JSON.stringify(savedNewsName);
+    localStorage.setItem("savedNews", updatedStorage);
+  }
+  
+  function removeNewsFromLocalStorage(newsName) {
+    const savedNewsName = getNewsFromLocalStorage();
+    localStorage.setItem(
+      "savedNews",
+      JSON.stringify(
+        savedNewsName.filter((newsNam) => newsNam.author !== newsName.author)
+      )
+    );
+  }
+// savedNews
+
+const showNotification = (message) => {
+    const alertMessage = document.createElement("h1");
+    alertMessage.classList.add("alertMessage");
+    alertMessage.innerText = message;
+    alertMessage.style.display = "flex";
+  
+    document.body.appendChild(alertMessage);
+  
+    setTimeout(() => {
+      alertMessage.style.display = "none";
+    }, 2000);
+  };
+  
+  //saveNews
+  const savedNewsList = getNewsFromLocalStorage();
+  const savedNews = document.getElementById("savedNews");
+  savedNews.innerHTML = "";
+  savedNewsList.map((news) => {
+    const { author, category, content, url } = news;
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = `
+          <div class="savedNews-content">
+            <div class="savedAuthor-category">
+              <h2 id="saved-author"><p>By</p> ${author}</h2>
+              <h2 id="saved-author"><p>Category</p> ${category}</h2>
+            </div>
+            <div class="savedNews-content-url">
+              <p id="saved-content">${content} <a id="saved-url" href="${url}">Read More.</a></p>
+             
+            </div>
+            <i class="fas fa-times-circle xmark"></i>
+          </div>
+        `;
+    savedNews.appendChild(newDiv);
+    const favIcon = newDiv.querySelector(".xmark");
+    favIcon.addEventListener("click", () => {
+      removeNewsFromLocalStorage(news);
+      newDiv.remove();
+      showNotification("1 Saved News item removed");
+    });
+  });
+  
+  document.querySelector(".new-icon").addEventListener("click", () => {
+    window.location.href = "../Newnews/newNews.html";
+  });
+  // }
